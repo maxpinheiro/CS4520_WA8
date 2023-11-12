c199c2d28475ca372b80f59c5f572b78474a44ce
Max Pinheiro <maxpinheiro181@gmail.com>
2023-11-11 21:01:48 -0500

    cleanup tableviewcell style

--- WA8_Pinheiro_1437/ChatList/ChatTableViewCell.swift
+++ WA8_Pinheiro_1437/ChatList/ChatTableViewCell.swift
@@ -24,7 +24,7 @@ class ChatTableViewCell: UITableViewCell {
     func setupWrapperCellView() {
         wrapperCellView = UITableViewCell()
         wrapperCellView.backgroundColor = .white
-        wrapperCellView.layer.cornerRadius = 10
+        wrapperCellView.layer.cornerRadius = 8
         wrapperCellView.layer.borderWidth = 1
         wrapperCellView.layer.borderColor = UIColor.systemGray.cgColor
         wrapperCellView.layer.shadowColor = UIColor.gray.cgColor
@@ -51,12 +51,12 @@ class ChatTableViewCell: UITableViewCell {
             wrapperCellView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
             wrapperCellView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
             
-            label.topAnchor.constraint(equalTo: wrapperCellView.topAnchor, constant: 12),
-            label.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 12),
-            label.heightAnchor.constraint(greaterThanOrEqualToConstant: 20),
-            label.trailingAnchor.constraint(equalTo: wrapperCellView.trailingAnchor, constant: -12),
+            label.topAnchor.constraint(equalTo: wrapperCellView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: wrapperCellView.bottomAnchor),
+            label.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 18),
+            label.trailingAnchor.constraint(equalTo: wrapperCellView.trailingAnchor, constant: -18),
             
-            wrapperCellView.heightAnchor.constraint(greaterThanOrEqualTo: label.heightAnchor, constant: 24)
+            wrapperCellView.heightAnchor.constraint(equalToConstant: 60)
         ])
         
     }
